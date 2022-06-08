bf090042da0ba493cd7565b4da821ccbd8d8ab21
luiizsilverio <luiiz.silverio@gmail.com>
2022-06-08 17:12:36 -0300

    Projeto concluído

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,10 @@ const cepInput = document.getElementById('cep')
 const addressInput = document.getElementById('address')
 const cityInput = document.getElementById('city')
 const neighborhoodInput = document.getElementById('neighborhood')
-const region = document.getElementById('region')
+const regionInput = document.getElementById('region')
 const formInputs = document.querySelectorAll('[data-input]')
 const closeButton = document.getElementById('close-message')
+const fadeElement = document.getElementById('fade')
 
 // Validate CEP input
 cepInput.addEventListener("keypress", (e) => {
@@ -31,15 +32,82 @@ cepInput.addEventListener("keyup", (e) => {
 // Get address from API
 const getAddress = async (cep) => {
   toggleLoader()
+
   cepInput.blur();
+
+  const apiUrl = `https://viacep.com.br/ws/${cep}/json/`
+
+  const response = await fetch(apiUrl)
+
+  const data = await response.json()
+
+  // console.log(data)
+  if (data.erro === "true") {
+    if (!addressInput.hasAttribute("disabled")) {
+      toggleDisabled()
+    }
+    addressForm.reset()
+    toggleLoader()
+    toggleMessage("CEP inválido, tente novamente.")
+    return
+  }
+
+  if (addressInput.value === "") {
+    toggleDisabled()
+  }
+
+  addressInput.value = data.logradouro
+  cityInput.value = data.localidade
+  neighborhoodInput.value = data.bairro
+  regionInput.value = data.uf
+
+  toggleLoader()
+
 };
+
+// Add or remove disabled attribute
+const toggleDisabled = () => {
+  if (regionInput.hasAttribute("disabled")) {
+    formInputs.forEach((input) => {
+      input.removeAttribute("disabled")
+    })
+  } else {
+    formInputs.forEach((input) => {
+      input.setAttribute("disabled", "disabled")
+    })
+  }
+}
 
 // Show or hide loader
 const toggleLoader = () => {
-  const fadeElement = document.getElementById('fade')
   const loaderElement = document.getElementById('loader')
-
-  console.log(loaderElement)
   fadeElement.classList.toggle('hide')
   loaderElement.classList.toggle('hide')
 }
+
+// Show or hide message
+const toggleMessage  = (msg) => {
+  const messageElement = document.getElementById('message')
+  const messageText = document.querySelector('#message p')
+
+  messageText.innerText = msg;
+
+  fadeElement.classList.toggle('hide')
+  messageElement.classList.toggle('hide')
+}
+
+// Close message modal
+closeButton.addEventListener("click", () => toggleMessage())
+
+// Save address
+addressForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  toggleLoader()
+
+  setTimeout(() => {
+    toggleLoader()
+    toggleMessage("Endereço salvo com sucesso!")
+    addressForm.reset()
+    toggleDisabled()
+  }, 1500)
+})
